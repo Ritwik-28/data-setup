3b248384014074cecc219ebf8c61325939755fab
Ritwik <ritwikgupta28@gmail.com>
2024-12-14 16:37:08 +0000

    Swagger Annotations Update

--- routes/crud.js
+++ routes/crud.js
@@ -2,7 +2,40 @@ const express = require('express');
 const pool = require('../db/pool');
 const router = express.Router();
 
-// CREATE
+/**
+ * @swagger
+ * /api/items:
+ *   post:
+ *     summary: Create a new item
+ *     description: Add a new item to the database.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: The name of the item.
+ *               value:
+ *                 type: string
+ *                 description: The value of the item.
+ *     responses:
+ *       201:
+ *         description: The created item.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                 name:
+ *                   type: string
+ *                 value:
+ *                   type: string
+ */
 router.post('/items', async (req, res) => {
     const { name, value } = req.body;
     try {
@@ -16,7 +49,32 @@ router.post('/items', async (req, res) => {
     }
 });
 
-// READ
+/**
+ * @swagger
+ * /api/items:
+ *   get:
+ *     summary: Retrieve all items
+ *     description: Get a list of all items from the database.
+ *     responses:
+ *       200:
+ *         description: A list of items.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     description: The item ID.
+ *                   name:
+ *                     type: string
+ *                     description: The item name.
+ *                   value:
+ *                     type: string
+ *                     description: The item value.
+ */
 router.get('/items', async (req, res) => {
     try {
         const result = await pool.query('SELECT * FROM items');
@@ -26,7 +84,45 @@ router.get('/items', async (req, res) => {
     }
 });
 
-// UPDATE
+/**
+ * @swagger
+ * /api/items/{id}:
+ *   put:
+ *     summary: Update an item
+ *     description: Update the details of an existing item.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: The ID of the item to update.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               value:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: The updated item.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                 name:
+ *                   type: string
+ *                 value:
+ *                   type: string
+ */
 router.put('/items/:id', async (req, res) => {
     const { id } = req.params;
     const { name, value } = req.body;
@@ -41,7 +137,23 @@ router.put('/items/:id', async (req, res) => {
     }
 });
 
-// DELETE
+/**
+ * @swagger
+ * /api/items/{id}:
+ *   delete:
+ *     summary: Delete an item
+ *     description: Remove an item from the database.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: The ID of the item to delete.
+ *     responses:
+ *       204:
+ *         description: No content (item deleted successfully).
+ */
 router.delete('/items/:id', async (req, res) => {
     const { id } = req.params;
     try {
@@ -53,3 +165,4 @@ router.delete('/items/:id', async (req, res) => {
 });
 
 module.exports = router;
+
